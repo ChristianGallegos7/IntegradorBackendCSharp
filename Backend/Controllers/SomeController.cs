ce909144ed3854cac84f5c854342c5e80b70d8f6
Christian <Christian@gmail.com>
2024-05-03 15:47:17 -0500

    Primer Commit

---
+++ Backend/Controllers/SomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SomeController : ControllerBase
+    {
+        [HttpGet("sync")]
+        public IActionResult GetSync()
+        {
+
+            Thread.Sleep(1000);
+            Console.WriteLine("Conexion to database ok");
+
+            Thread.Sleep(5000);
+            Console.WriteLine("Email enviado");
+            return Ok();
+        }
+
+        [HttpGet("sync")]
+        public async Task<IActionResult> GetAsync()
+        {
+            var taks1 = new Task( () =>
+            {
+
+                Thread.Sleep(1000);
+                Console.WriteLine("Conexion to database ok");
+
+            });
+
+            taks1.Start();
+
+            await taks1;
+        }
+    }
+}
